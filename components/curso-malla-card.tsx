2377ce4f3cb18195fc2d8dbc8dbba194961125ef
Pablo <pablocortes1101@gmail.com>
2025-09-19 17:11:49 -0300

    style: cambiar color curso-malla-card

--- components/curso-malla-card.tsx
+++ components/curso-malla-card.tsx
@@ -5,11 +5,11 @@ export function CursoMallaCard({ codigo, asignatura, creditos }: CursoMalla) {
   return (
     <Card className={`rounded-md p-0 w-36 h-20 shadow-sm overflow-hidden`}>
       <CardContent className="p-0 flex flex-col justify-start h-full">
-        <div className="h-8 bg-zinc-800 flex justify-between p-1">
-          <div className="w-5 h-5 bg-white rounded-full font-semibold flex justify-center items-center text-xs">
+        <div className="h-8 border-b flex justify-between p-1 items-center">
+          <div className="w-5 h-5 bg-zinc-900 text-white rounded-full font-semibold flex justify-center items-center text-xs">
             {creditos}
           </div>
-          <span className="font-semibold text-white text-sm">{codigo}</span>
+          <span className="font-semibold pr-1 text-xs">{codigo}</span>
         </div>
         <div className="px-1 flex flex-col justify-center items-center h-full">
           <p className="text-sm text-center text-wrap">{asignatura}</p>
