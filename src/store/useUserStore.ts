a209927f669dc8426b3b7b431276f51cd4bb63d1
Pablo <pablocortes1101@gmail.com>
2025-09-30 23:07:28 -0300

    feat: agregar proyecciones a userStore

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,14 +1,17 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { Carrera } from "../types/carrera";
+import { CursoProyeccion } from "../types/curso";
 
 interface UserState {
   rut: string;
   carreras: Carrera[];
   selectedCarrera: Carrera;
+  cursosProyeccion: CursoProyeccion[];
   setRut: (rut: string) => void;
   setCarreras: (carreras: Carrera[]) => void;
   setSelectedCarrera: (selectedCarrera: Carrera) => void;
+  setCursosProyeccion: (cursosProyeccion: CursoProyeccion[]) => void;
 }
 
 export const useUserStore = create<UserState>()(
@@ -17,9 +20,11 @@ export const useUserStore = create<UserState>()(
       rut: "",
       carreras: [],
       selectedCarrera: {} as Carrera,
+      cursosProyeccion: [],
       setRut: (rut) => set({ rut }),
       setCarreras: (carreras) => set({ carreras }),
       setSelectedCarrera: (selectedCarrera) => set({ selectedCarrera }),
+      setCursosProyeccion: (cursosProyeccion) => set({ cursosProyeccion }),
     }),
     { name: "user-storage" }
   )
