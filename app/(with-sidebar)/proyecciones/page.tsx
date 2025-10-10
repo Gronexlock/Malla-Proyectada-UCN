f3b878955a7885b4ab3e544c5deeed52e62dafcb
Diego Castro <117056665+Gronexlock@users.noreply.github.com>
2025-10-09 22:24:04 -0300

    Proyeccionesview

---
+++ app/(with-sidebar)/proyecciones/page.tsx
@@ -0,0 +1,9 @@
+import { ProyeccionesView } from "@/components/proyecciones-view";
+
+export default function Page() {
+  return (
+    <main className="p-4">
+      <ProyeccionesView />
+    </main>
+  );
+}
