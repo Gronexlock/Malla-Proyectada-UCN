5cd0b8e2b2f8eff15719e6b1039e3fea5cbc01be
Pablo <pablocortes1101@gmail.com>
2025-10-20 00:16:39 -0300

    refactor: crear hook para separar logica del diseño

--- components/crear-proyeccion-view.tsx
+++ components/crear-proyeccion-view.tsx
@@ -1,18 +1,13 @@
-import { CursoAvance, CursoMalla } from "@/src/types/curso";
 import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 import { romanNumerals } from "@/src/constants/numerosRomanos";
-import { useState, useEffect } from "react";
 import { CursoAvanceCard } from "./curso-avance-card";
 import { MallaSkeleton } from "./skeletons/malla-skeleton";
 import { Carrera } from "@/src/types/carrera";
 import { cn } from "@/lib/utils";
-import { getCursosPorAnio, getCursosPorNivel } from "@/src/utils/curso";
 import { getCursoStatus } from "@/src/utils/proyeccion";
-import { getSemestreActual, getSemestreSiguiente } from "@/src/utils/semestre";
 import { Button } from "./ui/button";
-import { useUserStore } from "@/src/store/useUserStore";
-import { Proyeccion } from "@/src/types/proyeccion";
 import { MoveLeft, MoveRight } from "lucide-react";
+import { useCrearProyeccion } from "@/hooks/use-proyeccion";
 
 type CrearProyeccionViewProps = {
   carrera: Carrera;
@@ -23,152 +18,20 @@ export function CrearProyeccionView({
   carrera,
   rut,
 }: CrearProyeccionViewProps) {
-  const [cursos, setCursos] = useState<CursoMalla[]>([]);
-  const [avance, setAvance] = useState<CursoAvance[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [altura, setAltura] = useState(0);
-  const cursosPorNivel = getCursosPorNivel(cursos);
-  const cursosPorAnio = getCursosPorAnio(cursosPorNivel);
-  const { proyecciones, setProyecciones } = useUserStore();
-  const [semestres, setSemestres] = useState([
-    getSemestreSiguiente(getSemestreActual()),
-  ]);
-  const [semestreIndex, setSemestreIndex] = useState(0);
-  const semestreActual = semestres[semestreIndex];
-  const [proyeccionesPorSemestre, setProyeccionesPorSemestre] = useState<
-    Record<string, CursoMalla[]>
-  >({});
-  const proyeccionActual = proyeccionesPorSemestre[semestreActual] || [];
-  const LIMITE_CREDITOS = 30;
-  const callbackRef = (node: HTMLDivElement | null) => {
-    if (node) setAltura(node.offsetHeight);
-  };
+  const proyeccion = useCrearProyeccion(carrera, rut);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!carrera || !rut) return;
-      setLoading(true);
-
-      try {
-        const [cursosResponse, avanceResponse] = await Promise.all([
-          fetch(`/api/mallas/?codigo=${carrera.codigo}-${carrera.catalogo}`),
-          fetch(`/api/avance/?rut=${rut}&codCarrera=${carrera.codigo}`),
-        ]);
-        const [cursosData, avanceData] = await Promise.all([
-          cursosResponse.json(),
-          avanceResponse.json(),
-        ]);
-        setCursos(cursosData);
-        setAvance(avanceData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setCursos([]);
-        setAvance([]);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [carrera, rut]);
-
-  if (loading) {
+  if (proyeccion.loading) {
     return <MallaSkeleton nombreCarrera={carrera.nombre.toLocaleLowerCase()} />;
-  }
-
-  function guardarProyecciones() {
-    const maxId = proyecciones.reduce((max, p) => (p.id > max ? p.id : max), 0);
-    const proyeccionesNueva: Proyeccion = {
-      id: maxId + 1,
-      proyecciones: [],
-    };
-
-    Object.keys(proyeccionesPorSemestre).forEach((semestre) => {
-      proyeccionesNueva.proyecciones.push({
-        semestre,
-        cursos: proyeccionesPorSemestre[semestre].map((curso) => ({
-          codigo: curso.codigo,
-          asignatura: curso.asignatura,
-          creditos: curso.creditos,
-          semestre,
-        })),
-      });
-    });
-    setProyecciones([...proyecciones, proyeccionesNueva]);
-  }
-
-  function toggleCursoProyeccion(curso: CursoMalla) {
-    setProyeccionesPorSemestre((prev) => {
-      const proyeccionActual = prev[semestreActual] || [];
-      const isCursoSelected = proyeccionActual.some(
-        (c) => c.codigo === curso.codigo
-      );
-
-      return {
-        ...prev,
-        [semestreActual]: isCursoSelected
-          ? proyeccionActual.filter((c) => c.codigo !== curso.codigo)
-          : [...proyeccionActual, curso],
-      };
-    });
-  }
-
-  function isAlreadySelected(codigo: string): boolean {
-    for (let i = 0; i < semestres.length; i++) {
-      const semestre = semestres[i];
-      const proyeccion = proyeccionesPorSemestre[semestre] || [];
-      if (proyeccion.some((c) => c.codigo === codigo)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  function irSiguienteSemestre() {
-    if (semestreIndex < semestres.length - 1) {
-      setSemestreIndex(semestreIndex + 1);
-    } else {
-      const ultimoSemestre = semestres[semestres.length - 1];
-      const siguienteSemestre = getSemestreSiguiente(ultimoSemestre);
-      setSemestres((prev) => [...prev, siguienteSemestre]);
-      setSemestreIndex(semestreIndex + 1);
-    }
-  }
-
-  function irSemestreAnterior() {
-    if (semestreIndex > 0) {
-      setSemestreIndex(semestreIndex - 1);
-    }
-  }
-
-  function getCreditosSemestreActual(): number {
-    return proyeccionActual.reduce((total, curso) => total + curso.creditos, 0);
-  }
-
-  function cumplePrerrequisitos(curso: CursoMalla): boolean {
-    if (!curso.prereq || curso.prereq.length === 0) return true;
-    return curso.prereq.every((pre) =>
-      avance.some((a) => a.course === pre.codigo && a.status === "APROBADO")
-    );
-  }
-
-  function getCursosBloqueantes(curso: CursoMalla) {
-    return curso.prereq.filter((pre) =>
-      cursos.some(
-        (c) =>
-          c.codigo === pre.codigo &&
-          getCursoStatus(c.codigo, avance) !== "APROBADO"
-      )
-    );
   }
 
   return (
     <div className="flex justify-center w-full">
       <ScrollArea className="min-w-0">
         <div
-          ref={callbackRef}
+          ref={proyeccion.callbackRef}
           className="inline-flex gap-4 p-6 pb-9 border border-r-0 rounded-l-lg bg-zinc-100"
         >
-          {Object.keys(cursosPorAnio)
+          {Object.keys(proyeccion.cursosPorAnio)
             .sort((a, b) => Number(a) - Number(b))
             .map((anio) => (
               <div key={anio} className="flex flex-col gap-2">
@@ -176,22 +39,25 @@ export function CrearProyeccionView({
                   Año {anio}
                 </div>
                 <div className="flex gap-4">
-                  {cursosPorAnio[Number(anio)].map((level) => (
+                  {proyeccion.cursosPorAnio[Number(anio)].map((level) => (
                     <div key={level} className="flex flex-col gap-2">
                       <div className="bg-zinc-400 rounded-sm flex justify-center items-center mb-2">
                         <h2 className="text-center font-semibold">
                           {romanNumerals[level]}
                         </h2>
                       </div>
-                      {cursosPorNivel[level].map((course) => {
-                        const status = getCursoStatus(course.codigo, avance);
-                        const alreadySelected = isAlreadySelected(
+                      {proyeccion.cursosPorNivel[level].map((course) => {
+                        const status = getCursoStatus(
+                          course.codigo,
+                          proyeccion.avance
+                        );
+                        const alreadySelected = proyeccion.isAlreadySelected(
                           course.codigo
                         );
                         const canBeSelected =
                           status !== "APROBADO" &&
                           !alreadySelected &&
-                          cumplePrerrequisitos(course);
+                          proyeccion.cumplePrerrequisitos(course);
                         status !== "APROBADO";
                         return (
                           <div className="relative">
@@ -201,12 +67,13 @@ export function CrearProyeccionView({
                                 "rounded-md border border-transparent",
                                 status === "APROBADO" && "opacity-50",
                                 status !== "APROBADO" &&
-                                  !cumplePrerrequisitos(course) &&
+                                  !proyeccion.cumplePrerrequisitos(course) &&
                                   "opacity-50"
                               )}
                               onClick={
                                 canBeSelected
-                                  ? () => toggleCursoProyeccion(course)
+                                  ? () =>
+                                      proyeccion.toggleCursoProyeccion(course)
                                   : undefined
                               }
                             >
@@ -216,7 +83,9 @@ export function CrearProyeccionView({
                                 creditos={course.creditos}
                                 status={alreadySelected ? "PENDIENTE" : status}
                                 prereq={course.prereq}
-                                bloqueantes={getCursosBloqueantes(course)}
+                                bloqueantes={proyeccion.getCursosBloqueantes(
+                                  course
+                                )}
                                 clickable={canBeSelected}
                               />
                             </div>
@@ -233,32 +102,41 @@ export function CrearProyeccionView({
       </ScrollArea>
       <div
         className={`w-64 shrink-0 flex flex-col bg-white border border-l-0 rounded-r-lg items-center p-4 text-wrap text-center`}
-        style={{ maxHeight: `${altura}px`, height: `${altura}px` }}
+        style={{
+          maxHeight: `${proyeccion.altura}px`,
+          height: `${proyeccion.altura}px`,
+        }}
       >
         <div>
-          <h2 className="font-bold text-lg">{semestreActual}</h2>
+          <h2 className="font-bold text-lg">{proyeccion.semestreActual}</h2>
           <div
             className={cn(
               "px-2 py-1 rounded-full text-white text-sm font-semibold transition-colors mt-1 mb-4 max-w-fit",
               {
-                "bg-zinc-900": getCreditosSemestreActual() < LIMITE_CREDITOS,
-                "bg-amber-500": getCreditosSemestreActual() === LIMITE_CREDITOS,
-                "bg-red-600": getCreditosSemestreActual() > LIMITE_CREDITOS,
+                "bg-zinc-900":
+                  proyeccion.getCreditosSemestreActual() <
+                  proyeccion.LIMITE_CREDITOS,
+                "bg-amber-500":
+                  proyeccion.getCreditosSemestreActual() ===
+                  proyeccion.LIMITE_CREDITOS,
+                "bg-red-600":
+                  proyeccion.getCreditosSemestreActual() >
+                  proyeccion.LIMITE_CREDITOS,
               }
             )}
           >
-            Créditos: {getCreditosSemestreActual()}
+            Créditos: {proyeccion.getCreditosSemestreActual()}
           </div>
         </div>
         <div className="mb-4 flex flex-col items-center flex-1 w-full h-full overflow-y-auto">
-          {proyeccionActual.length === 0 && (
+          {proyeccion.proyeccionActual.length === 0 && (
             <div className="text-gray-400 text-sm ">
               Selecciona cursos pendientes o reprobados para agregarlos aquí.
             </div>
           )}
           <div className="w-full">
             <ul className="space-y-4">
-              {proyeccionActual.map((curso) => (
+              {proyeccion.proyeccionActual.map((curso) => (
                 <li
                   key={curso.codigo}
                   className="relative group flex justify-center"
@@ -267,10 +145,10 @@ export function CrearProyeccionView({
                     asignatura={curso.asignatura}
                     codigo={curso.codigo}
                     creditos={curso.creditos}
-                    status={getCursoStatus(curso.codigo, avance)}
+                    status={getCursoStatus(curso.codigo, proyeccion.avance)}
                     prereq={curso.prereq}
-                    bloqueantes={getCursosBloqueantes(curso)}
-                    onClick={() => toggleCursoProyeccion(curso)}
+                    bloqueantes={proyeccion.getCursosBloqueantes(curso)}
+                    onClick={() => proyeccion.toggleCursoProyeccion(curso)}
                     clickable
                   />
                 </li>
@@ -283,28 +161,29 @@ export function CrearProyeccionView({
             <div className="flex justify-between gap-4">
               <Button
                 className="cursor-pointer mt-4"
-                onClick={irSemestreAnterior}
-                disabled={semestreIndex === 0}
+                onClick={proyeccion.irSemestreAnterior}
+                disabled={proyeccion.semestreIndex === 0}
               >
                 <MoveLeft />
                 Anterior
               </Button>
               <Button
                 className="cursor-pointer mt-4"
-                onClick={irSiguienteSemestre}
-                disabled={proyeccionActual.length === 0}
+                onClick={proyeccion.irSiguienteSemestre}
+                disabled={proyeccion.proyeccionActual.length === 0}
               >
                 Siguiente
                 <MoveRight />
               </Button>
             </div>
             <Button
-              onClick={guardarProyecciones}
+              onClick={proyeccion.guardarProyecciones}
               className="w-full cursor-pointer"
               variant="default"
               disabled={
-                proyeccionActual.length === 0 ||
-                getCreditosSemestreActual() > LIMITE_CREDITOS
+                proyeccion.proyeccionActual.length === 0 ||
+                proyeccion.getCreditosSemestreActual() >
+                  proyeccion.LIMITE_CREDITOS
               }
             >
               Guardar Proyección
---
+++ hooks/use-proyeccion.ts
@@ -0,0 +1,172 @@
+import { CursoAvance, CursoMalla } from "@/src/types/curso";
+import { useState, useEffect } from "react";
+import { Carrera } from "@/src/types/carrera";
+import { getCursosPorAnio, getCursosPorNivel } from "@/src/utils/curso";
+import { getCursoStatus } from "@/src/utils/proyeccion";
+import { getSemestreActual, getSemestreSiguiente } from "@/src/utils/semestre";
+import { useUserStore } from "@/src/store/useUserStore";
+import { Proyeccion } from "@/src/types/proyeccion";
+
+export function useCrearProyeccion(carrera: Carrera, rut: string) {
+  const [cursos, setCursos] = useState<CursoMalla[]>([]);
+  const [avance, setAvance] = useState<CursoAvance[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [altura, setAltura] = useState(0);
+  const cursosPorNivel = getCursosPorNivel(cursos);
+  const cursosPorAnio = getCursosPorAnio(cursosPorNivel);
+  const { proyecciones, setProyecciones } = useUserStore();
+  const [semestres, setSemestres] = useState([
+    getSemestreSiguiente(getSemestreActual()),
+  ]);
+  const [semestreIndex, setSemestreIndex] = useState(0);
+  const semestreActual = semestres[semestreIndex];
+  const [proyeccionesPorSemestre, setProyeccionesPorSemestre] = useState<
+    Record<string, CursoMalla[]>
+  >({});
+  const proyeccionActual = proyeccionesPorSemestre[semestreActual] || [];
+  const LIMITE_CREDITOS = 30;
+  const callbackRef = (node: HTMLDivElement | null) => {
+    if (node) setAltura(node.offsetHeight);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!carrera || !rut) return;
+      setLoading(true);
+
+      try {
+        const [cursosResponse, avanceResponse] = await Promise.all([
+          fetch(`/api/mallas/?codigo=${carrera.codigo}-${carrera.catalogo}`),
+          fetch(`/api/avance/?rut=${rut}&codCarrera=${carrera.codigo}`),
+        ]);
+        const [cursosData, avanceData] = await Promise.all([
+          cursosResponse.json(),
+          avanceResponse.json(),
+        ]);
+        setCursos(cursosData);
+        setAvance(avanceData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setCursos([]);
+        setAvance([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [carrera, rut]);
+
+  function guardarProyecciones() {
+    const maxId = proyecciones.reduce((max, p) => (p.id > max ? p.id : max), 0);
+    const proyeccionesNueva: Proyeccion = {
+      id: maxId + 1,
+      proyecciones: [],
+    };
+
+    Object.keys(proyeccionesPorSemestre).forEach((semestre) => {
+      proyeccionesNueva.proyecciones.push({
+        semestre,
+        cursos: proyeccionesPorSemestre[semestre].map((curso) => ({
+          codigo: curso.codigo,
+          asignatura: curso.asignatura,
+          creditos: curso.creditos,
+          semestre,
+        })),
+      });
+    });
+    setProyecciones([...proyecciones, proyeccionesNueva]);
+  }
+
+  function toggleCursoProyeccion(curso: CursoMalla) {
+    setProyeccionesPorSemestre((prev) => {
+      const proyeccionActual = prev[semestreActual] || [];
+      const isCursoSelected = proyeccionActual.some(
+        (c) => c.codigo === curso.codigo
+      );
+
+      return {
+        ...prev,
+        [semestreActual]: isCursoSelected
+          ? proyeccionActual.filter((c) => c.codigo !== curso.codigo)
+          : [...proyeccionActual, curso],
+      };
+    });
+  }
+
+  function isAlreadySelected(codigo: string): boolean {
+    for (let i = 0; i < semestres.length; i++) {
+      const semestre = semestres[i];
+      const proyeccion = proyeccionesPorSemestre[semestre] || [];
+      if (proyeccion.some((c) => c.codigo === codigo)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function irSiguienteSemestre() {
+    if (semestreIndex < semestres.length - 1) {
+      setSemestreIndex(semestreIndex + 1);
+    } else {
+      const ultimoSemestre = semestres[semestres.length - 1];
+      const siguienteSemestre = getSemestreSiguiente(ultimoSemestre);
+      setSemestres((prev) => [...prev, siguienteSemestre]);
+      setSemestreIndex(semestreIndex + 1);
+    }
+  }
+
+  function irSemestreAnterior() {
+    if (semestreIndex > 0) {
+      setSemestreIndex(semestreIndex - 1);
+    }
+  }
+
+  function getCreditosSemestreActual(): number {
+    return proyeccionActual.reduce((total, curso) => total + curso.creditos, 0);
+  }
+
+  function cumplePrerrequisitos(curso: CursoMalla): boolean {
+    if (!curso.prereq || curso.prereq.length === 0) return true;
+    return curso.prereq.every((pre) =>
+      avance.some((a) => a.course === pre.codigo && a.status === "APROBADO")
+    );
+  }
+
+  function getCursosBloqueantes(curso: CursoMalla) {
+    return curso.prereq.filter((pre) =>
+      cursos.some(
+        (c) =>
+          c.codigo === pre.codigo &&
+          getCursoStatus(c.codigo, avance) !== "APROBADO"
+      )
+    );
+  }
+
+  return {
+    cursos,
+    avance,
+    loading,
+    altura,
+    setAltura,
+    cursosPorNivel,
+    cursosPorAnio,
+    proyecciones,
+    setProyecciones,
+    semestres,
+    semestreIndex,
+    semestreActual,
+    proyeccionesPorSemestre,
+    setProyeccionesPorSemestre,
+    proyeccionActual,
+    LIMITE_CREDITOS,
+    callbackRef,
+    guardarProyecciones,
+    toggleCursoProyeccion,
+    isAlreadySelected,
+    irSiguienteSemestre,
+    irSemestreAnterior,
+    getCreditosSemestreActual,
+    cumplePrerrequisitos,
+    getCursosBloqueantes,
+  };
+}
