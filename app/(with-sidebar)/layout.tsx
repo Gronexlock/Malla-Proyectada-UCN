83c0bac047f2926afb613c4db1aebe3ef4ea5ba3
Renata <renata.cuello@alumnos.ucn.cl>
2025-11-22 01:14:31 -0300

    Agregar nombre de la carrera como header en el layout principal
    falta ponerle una letra más bonita

--- app/(with-sidebar)/layout.tsx
+++ app/(with-sidebar)/layout.tsx
@@ -2,6 +2,13 @@ import "../globals.css";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { cookies } from "next/headers";
+import { getUser } from "@/src/actions/cookiesActions";
+import { Geist} from "next/font/google";
+
+const geistSans = Geist({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
 
 export default async function RootLayout({
   children,
@@ -10,12 +17,32 @@ export default async function RootLayout({
 }>) {
   const cookieStore = await cookies();
   const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+  const { selectedCarrera } = await getUser();
 
   return (
     <SidebarProvider defaultOpen={defaultOpen}>
       <AppSidebar />
       <main className="flex-1 w-full overflow-x-hidden">
         <SidebarTrigger />
+        <header className="site-header">
+          <div className="container flex items-center justify-center">
+            {selectedCarrera?.nombre === "ITI" && (
+              <h1 className={`${geistSans.className} text-2xl font-semibold`}>
+                Ingeniería en Tecnologías de Información</h1>
+            )}
+            {selectedCarrera?.nombre === "ICI" && (
+              <h1 className={`${geistSans.className} text-2xl font-semibold`}>
+                Ingeniería Civil Industrial</h1>
+            )}
+            {selectedCarrera?.nombre === "ICCI" && (
+              <h1 className={`${geistSans.className} text-2xl font-semibold`}>
+                Ingeniería Civil en Computación e Informática</h1>
+            )}
+            {!selectedCarrera && 
+              <h1 className={`${geistSans.className} text-2xl font-semibold`}>
+                Carrera no definida</h1>}
+          </div>
+        </header>
         {children}
       </main>
     </SidebarProvider>
