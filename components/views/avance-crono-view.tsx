7c7f4acb479b0340e20c841a428b58fce8745522
Pablo <pablocortes1101@gmail.com>
2025-12-06 00:31:47 -0300

    style: ajustar diseño de avance crono para seguir tema de la apicacion

--- components/views/avance-crono-view.tsx
+++ components/views/avance-crono-view.tsx
@@ -1,7 +1,6 @@
 import { Curso } from "@/src/types/curso";
 import { getCursosPorPeriodo } from "@/src/utils/cursosUtils";
 import { formatPeriod } from "@/src/utils/semestreUtils";
-import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 import CursoCard from "../curso/curso-card";
 
 type AvanceCronoViewProps = {
@@ -12,18 +11,34 @@ export function AvanceCronoView({ cursos }: AvanceCronoViewProps) {
   const cursosPorSemestre = getCursosPorPeriodo(cursos);
 
   return (
-    <ScrollArea className="w-full whitespace-nowrap h-[calc(100vh-64px)]">
+    <div className="w-full whitespace-nowrap overflow-scroll">
+      <div className="flex flex-col min-w-max pb-4">
+        <div className="flex gap-2 ">
+          <div className="flex items-center gap-1">
+            <div className="size-3 bg-emerald-500/70 dark:bg-emerald-500/50 rounded-full"></div>
+            <p className="text-xs text-muted-foreground">Aprobado</p>
+          </div>
+          <div className="flex items-center gap-1">
+            <div className="size-3 bg-red-500/70 dark:bg-red-500/50 rounded-full"></div>
+            <p className="text-xs text-muted-foreground">Reprobado</p>
+          </div>
+          <div className="flex items-center gap-1">
+            <div className="size-3 bg-yellow-500/70 dark:bg-yellow-500/50 rounded-full"></div>
+            <p className="text-xs text-muted-foreground">Inscrito</p>
+          </div>
+        </div>
+      </div>
       <div className="flex justify-center min-w-max gap-4 pb-6 pr-6">
         {Object.keys(cursosPorSemestre)
           .sort((a, b) => Number(a) - Number(b))
           .map((semestre) => (
-            <div key={semestre} className="flex flex-col gap-2">
-              <div className="bg-zinc-800 rounded-sm flex justify-center items-center mb-2">
-                <h2 className="text-center text-white font-semibold">
+            <div key={semestre} className="flex flex-col gap-4">
+              <div className="rounded flex justify-center items-center bg-muted p-1">
+                <h2 className="text-center font-semibold">
                   {formatPeriod(semestre)}
                 </h2>
               </div>
-              <div className="flex flex-col gap-2">
+              <div className="flex flex-col gap-4">
                 {cursosPorSemestre[semestre].map((curso) => (
                   <CursoCard key={curso.nrc} curso={curso} />
                 ))}
@@ -31,7 +46,6 @@ export function AvanceCronoView({ cursos }: AvanceCronoViewProps) {
             </div>
           ))}
       </div>
-      <ScrollBar orientation="horizontal" />
-    </ScrollArea>
+    </div>
   );
 }
