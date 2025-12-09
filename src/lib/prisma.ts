e102d62bec075aa5c71a5315122b6c708959caef
Pablo <pablocortes1101@gmail.com>
2025-12-08 21:03:29 -0300

    fix: arreglar import de cliente prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../generated/prisma/client";
 
 const globalForPrisma = global as any;
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     datasourceUrl: process.env.DATABASE_URL,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
